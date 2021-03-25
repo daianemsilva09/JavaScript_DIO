456d0ee7ebdeb282be713ac5542e1d82c12e4ec5
Daiane Silva <daianemsilva09@gmail.com>
2021-03-25 16:19:29 -0300

    primeira aula ok

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var nome = "Daiane Silva";
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão melhor time";
+
+//alert (idade + idade2);
+//alert ("Bem vindo" + nome);
+
+console.log(nome);
+console.log(idade + idade2);
+//console.log(frase.replace("Japão", "Brasil"));
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());
