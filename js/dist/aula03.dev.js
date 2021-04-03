2447208e235581192a3da8300f995f7ff70fa652
Daiane Silva <daianemsilva09@gmail.com>
2021-04-02 23:02:17 -0300

    commit -m "teste"

---
+++ js/dist/aula03.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var notas = [100, 50, 20, 10, 5, 2, 1];
+var nnotas = [0, 0, 0, 0, 0, 0, 0];
+var quantia = parseInt(gets());
+var resto = quantia;
+
+while (resto >= 1) {
+  nota = notas.findIndex(function (value) {
+    return value <= resto;
+  });
+  nnotas[nota] = Math.trunc(resto / notas[nota]);
+  resto = resto % notas[nota];
+}
+
+console.log(quantia);
+console.log(nnotas[0] + " nota(s) de R$ 100,00");
+console.log(nnotas[1] + " nota(s) de R$ 50,00");
+console.log(nnotas[2] + " nota(s) de R$ 20,00");
+console.log(nnotas[3] + " nota(s) de R$ 10,00");
+console.log(nnotas[4] + " nota(s) de R$ 5,00");
+console.log(nnotas[5] + " nota(s) de R$ 2,00");
+console.log(nnotas[6] + " nota(s) de R$ 1,00");
